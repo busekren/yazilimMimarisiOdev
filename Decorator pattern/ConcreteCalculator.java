7d0903f64d9eff1c8f8f769b82b8569d2f564678
Şafak Erer <59174179+safakerer@users.noreply.github.com>
2019-12-23 21:12:39 +0300

    Add files via upload

---
+++ Decorator pattern/ConcreteCalculator.java
@@ -0,0 +1,10 @@
+public class ConcreteCalculator implements Calculator {
+    private double value=0;
+    public ConcreteCalculator(double value){
+        this.value=value;
+    }
+    @Override
+    public double calculate() {
+        return value;
+    }
+}
